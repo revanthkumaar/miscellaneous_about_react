650149421a91324dc8fd3c6c5dd5ed5efb9c6023
revanthkumaar <iamrevanthkumar@gmail.com>
2021-03-05 11:18:56 +0530

    laskjdlkj
    ljalskjdla

--- misc-react-concepts/src/index.js
+++ misc-react-concepts/src/index.js
@@ -4,8 +4,91 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-//simplest way of writing a component - "function component"
+/*
 
+//state and lifecycle
+function time(){
+  const element = (
+    <div>
+      <h1>clock</h1>
+      <h2>now the time is {new Date().toLocaleTimeString()}</h2>
+    </div>
+  );
+  ReactDOM.render(element,document.getElementById('root'))
+}
+
+setInterval(time,1000)
+
+//converting the above code and seggregating it
+function Clock(props) {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {props.date.toLocaleTimeString()}.</h2>
+    </div>
+  );
+}
+
+
+function tick(){
+  ReactDOM.render(
+    <Clock date={new Date()}/>,
+    document.getElementById('root')
+  );
+}
+
+setInterval(tick,1000)
+
+
+//converting a function component to a class Component
+
+
+class Clock extends React.Component{
+  render(){
+    return (
+      <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+    </div>
+    );
+  }
+}
+
+//add local state to a class
+
+
+
+class Clock extends React.Component{
+  constructor(props){
+    super(props); //class components should always call the base contructor with props
+    this.state = {date: new Date()}
+  }
+
+  render(){
+    return (
+      <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+    </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Clock/>,
+  document.getElementById('root')
+);
+
+
+
+
+
+
+
+
+
+//simplest way of writing a component - "function component"
+/*
 function Welcome(props){
   return <h1>hello, {props.name} </h1>
 }
